8e8ab7848b88b7bbacaef4c9becce9b44e83df01
Gabriela Lewenfus <gabriela.lewenfus@gmail.com>
2020-08-11 13:13:27 -0300

    inclui jobs de escala e graphics no setup

--- codigo/setup.py
+++ codigo/setup.py
@@ -20,7 +20,16 @@ setup(
     entry_points={
         'console_scripts': [
             'prepara-pedidos=desafio_iafront.jobs.pedidos:main',
-            'cria-visitas=desafio_iafront.jobs.create_visits:main'
+            'cria-visitas=desafio_iafront.jobs.create_visits:main',
+            'normalize=desafio_iafront.jobs.escala_pedidos.job_normalizacao:main',
+            'max-abs-scaler=desafio_iafront.jobs.escala_pedidos.job_max_abs_scaler:main',
+            'min-max-scaler=desafio_iafront.jobs.escala_pedidos.job_min_max_scaler:main',
+            'standard-scaler=desafio_iafront.jobs.escala_pedidos.job_standard_scaler:main',
+            'power-transformer=desafio_iafront.jobs.escala_pedidos.job_power_transformer:main',
+            'robust-scaler=desafio_iafront.jobs.escala_pedidos.job_robust_scaler:main',
+            'scatter=desafio_iafront.jobs.graphics.job_scatter:main',
+            'histogram=desafio_iafront.jobs.graphics.job_histogram:main',
+            'graphic-clusters=desafio_iafront.jobs.graphics.job_graphics:main'
         ]
     }
 )
