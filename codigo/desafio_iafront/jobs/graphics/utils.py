1fb8bd78572038432a15597d433424dbd3f8f4cf
Gabriela Lewenfus <gabriela.lewenfus@gmail.com>
2020-08-16 21:10:11 -0300

    inclui funcoes auxiliares da funcao main

--- codigo/desafio_iafront/jobs/graphics/utils.py
+++ codigo/desafio_iafront/jobs/graphics/utils.py
@@ -11,7 +11,7 @@ def plot(dataframe: pd.DataFrame, x_axis, y_axis, cluster_label, title=""):
 
     p = figure(title=title)
 
-    p.scatter(dataframe[x_axis].tolist(), dataframe[y_axis].tolist(), fill_color=colors)
+    p.scatter(dataframe[x_axis].tolist(), dataframe[y_axis].tolist(), fill_color=colors, alpha=0.5)
 
     return p
 
@@ -19,25 +19,22 @@ def plot_clusters(dataframe: pd.DataFrame, x_axis, y_axis, cluster_label, title=
 
     clusters = dataframe[cluster_label].unique()
     colors = [set_color(_) for _ in clusters]
-    colors=factor_cmap(cluster_label, palette=colors, factors=np.sort(clusters))
-        
-    print(clusters, colors)
+    colors = factor_cmap(cluster_label, palette=colors, factors=np.sort(clusters))        
+    
     p = figure(title=title)
 
-    p.scatter(x_axis, y_axis,  source=dataframe,  fill_color=colors, legend=cluster_label)
+    p.scatter(x_axis, y_axis,  source=dataframe,  fill_color=colors, alpha=0.5, legend=cluster_label)
 
     return p
 
 def hist(dataframe: pd.DataFrame, x_axis, title=""):
     p = figure(title=title)
 
-    hist, edges = np.histogram(dataframe[x_axis], density=True, bins=30)
+    hist, edges = np.histogram(dataframe[x_axis], density=True, bins=20)
     p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
            fill_color="navy", line_color="white", alpha=0.5)
 
     p.y_range.start = 0
-    p.legend.location = "center_right"
-    p.legend.background_fill_color = "#fefefe"
     p.xaxis.axis_label = x_axis
     p.yaxis.axis_label = 'Frequência'
     p.grid.grid_line_color="white"
