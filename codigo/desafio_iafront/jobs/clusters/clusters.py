780a5178dbd2edee4fcad97245bb21df7b42c1f5
Gabriela Lewenfus <gabriela.lewenfus@gmail.com>
2020-08-16 21:07:59 -0300

    inclui novos algs de clauter

--- codigo/desafio_iafront/jobs/clusters/clusters.py
+++ codigo/desafio_iafront/jobs/clusters/clusters.py
@@ -1,9 +1,31 @@
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN, MiniBatchKMeans
 
 
 def kmeans(vector: np.array, n: int):
     k = KMeans(n_clusters=n, random_state=0)
+    cluster_coordinate = k.fit_transform(vector)
+    cluster_label = k.fit(vector)
+
+    return cluster_coordinate, cluster_label.labels_
+
+
+def agg_clustering(vector: np.array, n: int):
+    k = AgglomerativeClustering(linkage='ward', n_clusters=n)
+   
+    cluster_label = k.fit(vector)
+
+    return None, cluster_label.labels_
+
+def dbscan(vector: np.array, n: int):
+    k = DBSCAN()
+    cluster_label = k.fit(vector)
+
+    return None, cluster_label.labels_
+
+
+def minbatchkmeans(vector: np.array, n: int):
+    k = MiniBatchKMeans(n_clusters=n, random_state=0)
     cluster_coordinate = k.fit_transform(vector)
     cluster_label = k.fit(vector)
 
