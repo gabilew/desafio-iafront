a6dd3a17c8e311efdcd9e7bf97a2e698316ff442
Gabriela Lewenfus <gabriela.lewenfus@gmail.com>
2020-08-16 21:09:00 -0300

    inclui departamentos

--- codigo/desafio_iafront/jobs/contants.py
+++ codigo/desafio_iafront/jobs/contants.py
@@ -5,3 +5,5 @@ COLUNA_DEPARTAMENTO = "departamento"
 COLUNA_LATITUDE = "geolocation_lat"
 COLUNA_LONGITUDE = "geolocation_lng"
 PREFIXO_CEP_RAW = "geolocation_zip_code_prefix"
+DEPARTAMENTOS="moveis_sala,eletronicos,perfumaria,dvds_blu_ray,nan,construcao_ferramentas_seguranca,casa_conforto_2,eletrodomesticos_2,artes_e_artesanato,pc_gamer,moveis_decoracao,musica"
+	
