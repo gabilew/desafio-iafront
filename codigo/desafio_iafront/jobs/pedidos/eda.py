a28710f2421f5227767543d2e9e9fdfdff9b964f
Gabriela Lewenfus <gabriela.lewenfus@gmail.com>
2020-08-18 07:52:39 -0300

    inclui eda

---
+++ codigo/desafio_iafront/jobs/pedidos/eda.py
@@ -0,0 +1,87 @@
+import os
+import click
+import pandas as pd
+import numpy as np
+from datetime import timedelta
+
+
+from desafio_iafront.data.dataframe_utils import read_csv
+from utils import *
+from desafio_iafront.jobs.constants import DEPARTAMENTOS
+
+@click.command()
+@click.option('--pedidos', type=click.Path(exists=True))
+@click.option('--visitas', type=click.Path(exists=True))
+@click.option('--produtos', type=click.Path(exists=True))
+@click.option('--data-inicial', type=click.DateTime(formats=["%d/%m/%Y"]))
+@click.option('--data-final', type=click.DateTime(formats=["%d/%m/%Y"]))
+def main(pedidos, visitas, produtos, data_inicial, data_final):
+    produtos_df = read_csv(produtos)
+    produtos_df["product_id"] = produtos_df["product_id"].astype(str)
+
+    delta: timedelta = (data_final - data_inicial)
+    date_partitions = [data_inicial.date() + timedelta(days=days) for days in range(delta.days)]
+
+    count=0
+
+    for data in date_partitions:
+        hour_partitions = list(range(0, 24))
+    
+        for hour in hour_partitions: 
+            hour_snnipet = f"hora={hour}"
+            data_str = data.strftime('%Y-%m-%d')
+            date_partition = f"data={data_str}"  
+            print(f"EDA: {date_partition} {hour}h")
+            visitas_df = create_visitas_df(date_partition, hour_snnipet, visitas)    
+            pedidos_df = create_pedidos_df(date_partition, hour_snnipet, pedidos)    
+            visita_com_produto_e_conversao_df = merge_visita_produto(data_str, hour, pedidos_df, produtos_df, visitas_df)
+            nan_values = pd.DataFrame(visita_com_produto_e_conversao_df.isna().mean()*100)
+            nan=pd.DataFrame()
+            nan['cols'] = nan_values.index
+            nan['values']=nan_values.values
+            nan['index'] = 1
+            nan=nan.pivot(index='index',columns='cols')
+            try: 
+
+                missing_vals = pd.concat((missing_vals,nan ))
+            except: missing_vals = nan
+            
+            #visita_com_produto_e_conversao_df.rename(columns={"purchase_id":"id_pedido"}, inplace=True)
+            visita_com_produto_e_conversao_df["id_pedido"]=visita_com_produto_e_conversao_df.purchase_id
+            visita_com_produto_e_conversao_df=convert(visita_com_produto_e_conversao_df)
+            df = visita_com_produto_e_conversao_df
+         
+            #df['convertido'] = df.purchase_id.apply(lambda x: 1.0 if x!=np.nan else 0)
+            #print(df[['convertido', 'purchase_id']])
+            numerical_columns= list(filter(lambda x: "float64"==df[x].dtype or "int64"==df[x].dtype, df.columns))
+
+            df = df[numerical_columns]
+            try: 
+                corr += df.corr(method='spearman')
+            except: corr=df.corr(method='spearman')
+            count+=1
+            try:
+                summary = update_summary(summary, df.describe())
+            except:
+                summary = df.describe()
+            
+
+    corr = corr/count
+    corr.to_csv("correlation.csv")
+    summary.to_csv("summary.csv")
+    print(summary)
+    missing_vals.to_csv("missing_vals.csv")
+         
+         
+
+def update_summary(df1, df2):
+    count = df1.count + df2.count
+    mean = (df1.mean*df1.count + df2.mean*df2.count)/count
+    max_ = max([df1.max, df2.max])
+    min_ = min([df1.min, df2.min])
+    std = np.std((df1.std**2*df1.count + df2.std**2*df2.count)/count)
+    
+    return pd.DataFrame({'count':[count], 'mean':[mean], 'max':[max_], 'min':[min_], 'std':[std]})
+
+if __name__ == '__main__':
+    main()
