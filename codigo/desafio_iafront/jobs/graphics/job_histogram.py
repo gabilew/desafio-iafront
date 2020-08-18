1311f771a102aeacc1017b31bab3486dbc4ab004
Gabriela Lewenfus <gabriela.lewenfus@gmail.com>
2020-08-17 22:46:44 -0300

    fixa eixo x

--- codigo/desafio_iafront/jobs/graphics/job_histogram.py
+++ codigo/desafio_iafront/jobs/graphics/job_histogram.py
@@ -6,13 +6,14 @@ import os
 from desafio_iafront.jobs.graphics.utils import hist
 from desafio_iafront.data.dataframe_utils import read_partitioned_json
 from desafio_iafront.jobs.common import filter_date
+from desafio_iafront.jobs.constants import FEATURES
 from bokeh.plotting import figure, output_file
 from bokeh.layouts import gridplot
 
 @click.command()
 @click.option('--dataframe-path', type=click.Path(exists=True))
 @click.option('--saida', type=click.Path(exists=False, dir_okay=True, file_okay=False))
-@click.option('--x_axis')
+@click.option('--x_axis',  default=FEATURES)
 @click.option('--data-inicial', type=click.DateTime(formats=["%d/%m/%Y"]))
 @click.option('--data-final', type=click.DateTime(formats=["%d/%m/%Y"]))
 @click.option('--transform', default='')
@@ -21,15 +22,21 @@ def main(dataframe_path: str, saida: str, x_axis, data_inicial,  data_final, tra
     dataframe = read_partitioned_json(dataframe_path, filter_function=filter_function)
     convertido = dataframe[dataframe.convertido==1]
     
-    x_axis_lista =  x_axis.split(",")
+    if isinstance(x_axis,str):
+        x_axis_lista =  x_axis.split(",")
+    else:x_axis_lista = x_axis
     saida = os.path.split(saida)
     for x in x_axis_lista:
         filename = os.path.join(saida[0],x+'-'+saida[1])
         output_file(filename)
         print("Criando histograma em "+ filename)
-        p1 = hist(dataframe, x,   title="Original")
-        p2 = hist(dataframe, x+"_transformed",  title=transform)    
-        p3 = hist(convertido, x,   title="Original-Convertido")
+
+        x_range = (np.min(dataframe[x]), np.min(dataframe[x])) 
+        p1 = hist(dataframe, x, x_range, title="Original")
+        p3 = hist(convertido, x, x_range,  title="Original-Convertido")
+
+        x_range = (np.min(dataframe[x+"_transformed"]), np.min(dataframe[x+"_transformed"]))
+        p2 = hist(dataframe, x+"_transformed",  title=transform)          
         p4 = hist(convertido, x+"_transformed",  title=transform+"-Convertido")
 
         figura = gridplot([p1,p2,p3,p4], ncols=2,  plot_width=400, plot_height=400)
