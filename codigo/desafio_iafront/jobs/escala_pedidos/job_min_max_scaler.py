27eb4168cf6042f11c1a91b94ed51490d03fa675
Gabriela Lewenfus <gabriela.lewenfus@gmail.com>
2020-08-18 21:44:25 -0300

    inclui default para departamentos

--- codigo/desafio_iafront/jobs/escala_pedidos/job_min_max_scaler.py
+++ codigo/desafio_iafront/jobs/escala_pedidos/job_min_max_scaler.py
@@ -3,17 +3,19 @@ from sklearn.preprocessing import MinMaxScaler
 
 from desafio_iafront.data.saving import save_partitioned
 from desafio_iafront.jobs.common import prepare_dataframe, transform
-
+from desafio_iafront.jobs.constants import DEPARTAMENTOS 
 
 @click.command()
 @click.option('--visitas-com-conversao', type=click.Path(exists=True))
 @click.option('--saida', type=click.Path(exists=False, dir_okay=True, file_okay=False))
 @click.option('--data-inicial', type=click.DateTime(formats=["%d/%m/%Y"]))
 @click.option('--data-final', type=click.DateTime(formats=["%d/%m/%Y"]))
-@click.option('--departamentos', type=str, help="Departamentos separados por virgula")
+@click.option('--departamentos', type=str, defalut="", help="Departamentos separados por virgula")
 def main(visitas_com_conversao, saida, data_inicial, data_final, departamentos):
-    departamentos_lista = [departamento.strip() for departamento in departamentos.split(",")]
-
+    if len(departamentos) == 0:
+        departamentos_lista = DEPARTAMENTOS
+    else: 
+        departamentos_lista = [departamento.strip() for departamento in departamentos.split(",")]
     result = prepare_dataframe(departamentos_lista, visitas_com_conversao, data_inicial, data_final)
 
     # Faz a escala dos valores
